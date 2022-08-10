4f8ad515a70eeb558d0a2b82e70860ea5ecf730f
CJ <cj.pintacura@oracle.com>
2022-08-09 21:52:49 -0700

    ...

--- build.js
+++ build.js
@@ -3,17 +3,12 @@ const path = require('path')
 const indexFile = 'dist/index.html';
 
 (async () => {
-  console.log('fs', fs);
-  console.log('path', path);
-  console.log('indexFile', indexFile);
-  console.log('path.join(indexFile)', path.join(indexFile));
   let html = await fs.readFile(path.join(indexFile), 'utf8');
-  console.log('html', html);
   let TOML_FILE_VAR = process.env.TOML_FILE_VAR || '';
-  console.log('TOML_FILE_VAR', TOML_FILE_VAR);
   let SITE_ENV_VAR = process.env.SITE_ENV_VAR || '';
-  console.log('SITE_ENV_VAR', SITE_ENV_VAR);
   let INCOMING_HOOK_BODY  = process.env.INCOMING_HOOK_BODY || '';
+  console.log('TOML_FILE_VAR', TOML_FILE_VAR);
+  console.log('SITE_ENV_VAR', SITE_ENV_VAR);
   console.log('INCOMING_HOOK_BODY', INCOMING_HOOK_BODY);
   if (INCOMING_HOOK_BODY) {
     try {
@@ -26,12 +21,9 @@ const indexFile = 'dist/index.html';
   html = html.replace('<!--TOML_FILE_VAR-->', TOML_FILE_VAR);
   html = html.replace('<!--SITE_ENV_VAR-->', SITE_ENV_VAR);
   if (typeof INCOMING_HOOK_BODY === 'object') {
-    html = html.replace('<!--INCOMING_HOOK_BODY-->', JSON.stringify(INCOMING_HOOK_BODY, null, 2));
+    INCOMING_HOOK_BODY = JSON.stringify(INCOMING_HOOK_BODY, null, 2);
   }
-  else { // Just place the string value
-    html = html.replace('<!--INCOMING_HOOK_BODY-->', INCOMING_HOOK_BODY);
-  }
-  console.log('html', html);
+  html = html.replace('<!--INCOMING_HOOK_BODY-->', INCOMING_HOOK_BODY);
   await fs.writeFile(indexFile, html);
 })();
 
